d6cb6be5e8d3900a9847c9d924463311f2d8c3bd
Md Rashedul Islam <rajib5570@gmail.com>
2025-02-01 00:35:29 +0600

    [add] : register background image added

--- src/pages/public/Register.tsx
+++ src/pages/public/Register.tsx
@@ -61,114 +61,124 @@ const Register = () => {
   }, [error, isError, isSuccess,navigate]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
-      <Card className="w-full max-w-md shadow-xl">
-        <CardHeader className="text-center">
-          <CardTitle className="text-2xl font-bold">
-            Create an Account
-          </CardTitle>
-          <p className="text-gray-500 text-sm">Sign up to get started</p>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        autoComplete="name"
-                        placeholder="Your Name"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        autoComplete="email"
-                        placeholder="Enter email"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="image"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Profile Picture</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => field.onChange(e.target.files?.[0])}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        autoComplete="password"
-                        placeholder="password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button
-                type="submit"
-                className="w-full flex items-center justify-center gap-2"
-                disabled={isLoading}
+    <div className="relative min-h-screen bg-[url('/assets/register.jpg')] bg-cover bg-center flex justify-center items-center text-white px-6">
+      <div className="absolute inset-0 bg-gray-950/70"></div>
+      <div className="relative z-10 my-6 md:my-0 grid grid-cols-1 rounded-xl items-center">
+
+      
+      <div className="flex items-center justify-center">
+        <Card className="w-full max-w-md shadow-xl">
+          <CardHeader className="text-center">
+            <CardTitle className="text-2xl font-bold">
+              Create an Account
+            </CardTitle>
+            <p className="text-gray-500 text-sm">Sign up to get started</p>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
               >
-                {isLoading ? (
-                  <>
-                    <Gear />
-                    <span>Registering...</span>
-                  </>
-                ) : (
-                  "Register"
-                )}
-              </Button>
-            </form>
-          </Form>
-          <p className="text-center text-sm text-gray-600 mt-4">
-            Already have an account?
-            <span
-              className="text-indigo-600 cursor-pointer"
-              onClick={() => navigate("/login")}
-            >
-              {" "}
-              Log in
-            </span>
-          </p>
-        </CardContent>
-      </Card>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          autoComplete="name"
+                          placeholder="Your Name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          autoComplete="email"
+                          placeholder="Enter email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Profile Picture</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="file"
+                          accept="image/*"
+                          onChange={(e) => field.onChange(e.target.files?.[0])}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          autoComplete="password"
+                          placeholder="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  className="w-full flex items-center justify-center gap-2"
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <>
+                      <Gear />
+                      <span>Registering...</span>
+                    </>
+                  ) : (
+                    "Register"
+                  )}
+                </Button>
+              </form>
+            </Form>
+            <p className="text-center text-sm text-gray-600 mt-4">
+              Already have an account?
+              <span
+                className="text-indigo-600 cursor-pointer"
+                onClick={() => navigate("/login")}
+              >
+                {" "}
+                Log in
+              </span>
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+      </div>
     </div>
   );
 };
