7da070ea2f83cf7babffd7e1f0d079eeaf2a0278
Md Rashedul Islam <rajib5570@gmail.com>
2025-02-03 00:31:11 +0600

    [fix] :missing featured products added back

--- src/components/shared/FeaturedProducts.tsx
+++ src/components/shared/FeaturedProducts.tsx
@@ -4,10 +4,31 @@ import FeaturedCard from "./FeaturedCard";
 import { Button } from "../ui/button";
 import { TProduct } from "@/types/form.types";
 import { Link } from "react-router";
+import { useState } from "react";
 
 
 const FeaturedProducts = () => {
-    const {data, isLoading,isFetching} = useGetAllProductsQuery(undefined);
+    const [search] = useState("");
+      const [minPrice] = useState<number>(0);
+      const [maxPrice] = useState<number>(700000);
+      const [brand] = useState("");
+      const [category] = useState("");
+      const [availability] = useState("");
+    
+      const queryArgs = [
+        { name: "search", value: search },
+        { name: "minPrice", value: minPrice },
+        { name: "maxPrice", value: maxPrice },
+        { name: "brand", value: brand },
+        { name: "category", value: category },
+        { name: "inStock", value: availability },
+      ].filter(
+        (arg) => arg.value !== "" && arg.value !== "all" && arg.value !== null
+      );
+    
+      const { data, isLoading, isFetching } = useGetAllProductsQuery(queryArgs, {
+        // skip: true,
+      });
     
     console.log(isLoading);
     console.log(isFetching);
